7282f0d140e92b701f6197b3c55dadf0cbab8857
Dan Simmons <dan@simmons.io>
2022-09-04 13:54:08 -0600

    Added Next.js paths to Tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./public/**/*.html",
+  ],
   theme: {
     extend: {},
   },
