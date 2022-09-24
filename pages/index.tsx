c7f349ca72a4f563172430fa57c51e5a0346482f
Dan Simmons <dan@simmons.io>
2022-09-24 11:22:27 -0600

    Replaced favicon with new assets.

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,10 @@ const Home: NextPage = () => {
         <title>Greenhouse | Grow your web3 roots!</title>
         <meta name="description" content="Grow your web3 roots! Greenhouse helps you find and connect with like-minded frens on the Lens protocol." />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <main></main>
       <footer></footer>
