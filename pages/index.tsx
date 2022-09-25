34f031c8b361f5621ff6258cce8c324b5d745db2
Dan Simmons <dan@simmons.io>
2022-09-24 21:19:26 -0600

    Moved metadata section into own component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import { Button, Spinner } from 'flowbite-react'
 import { useState } from 'react'
-import Head from 'next/head'
 // import styles from '../styles/Home.module.css'
 
 import Header from '../src/components/Header'
+import Meta from '../src/components/Meta'
 import SearchResult from '../src/components/SearchResult'
 
 import * as api from '../src/api';
@@ -54,15 +54,7 @@ const Home: NextPage = () => {
 
   return (
     <div className="flex flex-col h-screen bg-slate-50">
-      <Head>
-        <title>Greenhouse | Grow your web3 roots!</title>
-        <meta name="description" content="Grow your web3 roots! Greenhouse helps you find and connect with like-minded frens on the Lens protocol." />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-      </Head>
+      <Meta />
       <Header />
       <main className="flex justify-center py-6">
         <section className="flex flex-col gap-3">
---
+++ src/components/Meta.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Meta() {
+  return (
+    <Head>
+      <title>Greenhouse | Grow your web3 roots!</title>
+      <meta name="description" content="Grow your web3 roots! Greenhouse helps you find and connect with like-minded frens on the Lens protocol." />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+    </Head>
+  )
+}
