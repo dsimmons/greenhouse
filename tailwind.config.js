17ff65a60ee3f9681107db215393499e94359653
Dan Simmons <dan@simmons.io>
2022-09-24 18:23:53 -0600

    Added flowbite-react.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./public/**/*.html",
+    './pages/**/*.{ts,tsx}',
+    './public/**/*.html',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
